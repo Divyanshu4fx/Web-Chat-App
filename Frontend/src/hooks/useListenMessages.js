ffde30c16e96a485840c50961c1f6706e2e813d0
Divyanshu4fx <divyanshustacks123@gmail.com>
2024-04-04 02:24:43 +0530

    Two Group Message Bug Resolved

--- Frontend/src/hooks/useListenMessages.js
+++ Frontend/src/hooks/useListenMessages.js
@@ -8,11 +8,10 @@ const useListenMessages = ()=>
     const { authUser } = useAuthContext();
    const {socket} = useSocketContex();
    const {messages,setMessages}= useConversation();
-    const 
    useEffect(()=>{
     socket?.on("newMessage",(newMessage)=>{
         if(newMessage.senderId === authUser._id)
-            return;
+          return;
         newMessage.shouldShake= true;
         const sound = new Audio(notificationSound);
         sound.play();
